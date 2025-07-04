7a3e73f2c768e56e9e678c1e0b8e6d3d541cb067
Justin Xie <justin.xie712@gmail.com>
2025-07-03 19:02:36 -0700

    refactor connection

--- server/app.js
+++ server/app.js
@@ -12,8 +12,13 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(3000, () =>
-    console.log("Server running on http://localhost:3000")
-  );
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(3000, () =>
+      console.log("Server running on http://localhost:3000")
+    );
+  });
