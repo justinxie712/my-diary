270fd20b8fec1577128047f37466c8737c8dc14e
Justin Xie <justin.xie712@gmail.com>
2025-07-07 20:18:59 -0700

    update packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const fetchDiaries = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:3000/api/entries", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NmM1ODU5Njg5MjYxOWMwMjVmYmUwNCIsImlhdCI6MTc1MTkzMDk2OX0.cJLcYNyJd92JzGM7QhTC1B3fz6Hg3ASmBxsBx6ZSS-o`, // 👈 include the JWT here
+          },
+        });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        const data = await res.json();
+        console.log("data", data);
+      } catch (err: any) {
+        console.error("Failed to fetch diaries:", err);
+      }
+    };
+
+    fetchDiaries();
+  }, []);
+  return <div>Test</div>;
 }
 
-export default App
+export default App;
