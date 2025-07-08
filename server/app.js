585157735e894c7a2246aa0157e500f843fd1136
Justin Xie <justin.xie712@gmail.com>
2025-07-07 18:26:01 -0700

    download sass

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes/index.js";
 
-dotenv.config();
+dotenv.config({ path: "./server/.env" });
 
 const app = express();
 app.use(cors());
